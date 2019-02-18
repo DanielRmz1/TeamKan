4856ddbd2362ebe55f2fc50613e4b0fa61aae18b
Manthra <daniel.1912@hotmail.com>
2019-02-18 17:25:16 -0600

    Primer Snapshot

---
+++ TeamKanBan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamKanBan.MvcApplication" Language="C#" %>
---
+++ TeamKanBan/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TeamKanBan.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TeamKanBan.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TeamKanBan.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
